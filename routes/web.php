6bebcb713eeea975eae74c59ce1e6428737503f0
Juan Manuel Torres <veinte2000jtm23@gmail.com>
2021-11-12 13:03:24 +0100

    Primeros pasos básicos
    - Creada una ruta Hola Mundo
    - Descomentada una línea de código para Laravel 8
    - Creado controlador básico para Hola Mundo

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*
+Route::get('/hola', function() {
+    return "Hola, mundo !!!!!";
+});
+*/
+
+Route::get('/hola', 'HolaController@index');
